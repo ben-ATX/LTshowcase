de334ffd7c84765fe0cca325151e792a281565bb
ben-atx <itsbenATX@gmail.com>
2024-09-13 17:54:27 -0500

    Add Test Project & ProductClient to handle requests to  https://dummyjson.com/products

---
+++ LTshowcase.Tests/Pages/Products/ProductClientTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using LTshowcase.Pages.Products.Services;
+
+namespace LTshowcase.Tests.Pages.Products;
+
+public class ProductClientTests
+{
+    private readonly IProductClient _productService = new ExternalApiProductClient(new HttpClient());
+
+    [Fact]
+    public async Task Should_find_product_by_id()
+    {
+        var query = new SearchQuery();
+        var allProducts = await _productService.Search(query, default);
+        var firstProduct = allProducts.Products.First();
+        var result = await _productService.GetById(firstProduct.Id, default);
+
+        result.Should().NotBeNull();
+        result.Id.Should().BeGreaterThan(0);
+    }
+
+    [Fact]
+    public async Task Should_not_find_product_by_id()
+    {
+        var result = await _productService.GetById(0, default);
+
+        result.Should().NotBeNull();
+        result.Id.Should().Be(0);
+        result.Title.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task Should_return_all_products_for_default_search()
+    {
+        var query = new SearchQuery();
+        var result = await _productService.Search(query, default);
+
+        result.Should().NotBeNull();
+
+        var totalProducts = result.Products.Count();
+        totalProducts.Should().BeGreaterThan(50);
+        result.Total.Should().Be(totalProducts);
+    }
+
+    [Fact]
+    public async Task Should_find_product_by_search_term()
+    {
+        var query = new SearchQuery { SearchTerm = "laptop" };
+        var result = await _productService.Search(query, default);
+
+        result.Should().NotBeNull();
+        
+        var totalProducts = result.Products.Count();
+        totalProducts.Should().BeGreaterThan(1);
+        result.Total.Should().Be(totalProducts);
+    }
+
+    [Fact]
+    public async Task Should_not_find_product_by_search_term()
+    {
+        var query = new SearchQuery { SearchTerm = "should not find a matching product" };
+        var result = await _productService.Search(query, default);
+
+        result.Should().NotBeNull();
+
+        var totalProducts = result.Products.Count();
+        totalProducts.Should().Be(0);
+        result.Total.Should().Be(totalProducts);
+    }
+}
---
+++ LTshowcase/Pages/Products/Services/ExternalApiProductClient.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using System.Text.Json;
+
+namespace LTshowcase.Pages.Products.Services;
+public interface IProductClient
+{
+    Task<Product?> GetById(int id, CancellationToken token);
+                 
+    Task<SearchResult?> Search(SearchQuery query, CancellationToken token);
+}
+public class ExternalApiProductClient : IProductClient
+{
+    private readonly HttpClient _httpClient;
+    public ExternalApiProductClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://dummyjson.com/products/");
+    }
+
+    public async Task<Product?> GetById(int id, CancellationToken token) =>
+        await Get<Product>(id.ToString(), token);
+
+    public async Task<SearchResult?> Search(SearchQuery query, CancellationToken token) => 
+        await Get<SearchResult>($"search?q={query.SearchTerm}&skip={query.Skip}&limit={query.Limit}", token);
+    
+    private async Task<T?> Get<T>(string requestUri, CancellationToken token) where T : class
+    {
+        var response = await _httpClient.GetAsync(requestUri, token);
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<T>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+        return result;
+    }
+}
+
+public record Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+    public decimal Price { get; set; }
+    public string Thumbnail { get; set; } = "";
+    public IEnumerable<string> Images { get; set; } = [];
+}
+
+public record SearchQuery : IRequest<SearchResult>
+{
+    public string SearchTerm { get; set; } = "";
+    public int Skip { get; set; }
+    public int Limit { get; set; }
+}
+
+public record SearchResult
+{
+    public string SearchTerm { get; set; } = "";
+    public int Skip { get; set; }
+    public int Limit { get; set; }
+    public int Total { get; set; }
+    public IEnumerable<Product> Products { get; set; } = [];
+}
